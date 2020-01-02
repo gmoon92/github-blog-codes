6bcdb2a3516e4e5209c8aa25183b702f982cf627
gmoon <gmoon@rsupport.com>
2020-01-02 14:57:27 +0900

    first commit by entity base audit demo project

---
+++ entity-audit-demo/src/main/java/com/moong/audit/EntityAuditDemoApplication.java
@@ -0,0 +1,13 @@
+package com.moong.audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityAuditDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityAuditDemoApplication.class, args);
+    }
+
+}
---
+++ entity-audit-demo/src/test/java/com/moong/audit/EntityAuditDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moong.audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityAuditDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
