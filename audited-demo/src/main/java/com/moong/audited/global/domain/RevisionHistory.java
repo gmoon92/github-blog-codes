ffb683ffc4cd0c98fddf81e01a742f5468ebc19c
gmoon <gmoon@rsupport.com>
2020-06-02 08:09:51 +0900

    개정 엔티티, 개정 이벤트 리스너 추가

--- audited-demo/src/main/java/com/moong/audited/global/domain/RevisionHistory.java
+++ audited-demo/src/main/java/com/moong/audited/global/domain/RevisionHistory.java
@@ -1,12 +1,17 @@
-package com.moong.audited.global;
+package com.moong.audited.global.domain;
 
 import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "REVISION_HISTORY")
 @RevisionEntity
 public class RevisionHistory extends DefaultRevisionEntity {
 
+    @Column(name = "username")
+    private String username;
 }
---
+++ audited-demo/src/main/java/com/moong/audited/global/envers/CustomRevisionListener.java
@@ -0,0 +1,11 @@
+package com.moong.audited.global.envers;
+
+import org.hibernate.envers.RevisionListener;
+
+public class CustomRevisionListener implements RevisionListener {
+
+    @Override
+    public void newRevision(Object revisionEntity) {
+
+    }
+}
